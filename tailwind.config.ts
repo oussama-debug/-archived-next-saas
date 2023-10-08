b0544309453d6d837f9f257a447d38497c2c78e3
flwatechnology <136766292+oussama-debug@users.noreply.github.com>
2023-10-08 14:37:04 -0400

    logos

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,15 @@ const config: Config = {
       sans: "var(--font-dm-sans), sans-serif",
     },
     colors: {
-      primary: "#000",
+      primary: "#211D2D",
+      accentPrimary: "#9C66FF",
+      secondary: "#13D3E9",
+    },
+    boxShadow: {
+      primary_button:
+        "0px 1px 1px 0px rgba(0, 0, 0, 0.12), 0px 0px 0px 1px rgba(89, 44, 229, 0.16), 0px 2px 5px 0px rgba(60, 66, 87, 0.08)",
+      secondary_button:
+        "0px 1px 1px 0px rgba(0, 0, 0, 0.12), 0px 0px 0px 1px rgba(60, 66, 87, 0.16), 0px 2px 5px 0px rgba(60, 66, 87, 0.08)",
     },
   },
   plugins: [],
